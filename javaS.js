d13c62994960caa67c58b7f26e7b3aa528da9ad2
sebamurgui <sebamurgui@gmail.com>
2016-06-02 10:02:35 -0300

    Tiempo agregado

--- javaS.js
+++ javaS.js
@@ -3,12 +3,12 @@
  */
 
  
-function comprobarNum(valorIngreso) {
+function comprobarNum(valorIngreso) {     //COMPRUEBA SI EL VALOR ES NUMERICO
     if (isNaN(valorIngreso)) {
         alert("Valor ingresado no numerico, ingresar de nuevo.")
     }
 }
- function convertirUnidades (valor, unidad){
+ function convertirUnidades (valor, unidad){ //CONVIERTE LAS UNIDADES DE VALORES AL CONVENCIONAL
      if(unidad == "miligramo" || unidad == "milimetro"){
          valor = valor/1000;
 
@@ -20,7 +20,7 @@ function comprobarNum(valorIngreso) {
      return valor;
  }
 
-    function valoresVelocidad (){
+    function valoresVelocidad (){          //CONCATENA LOS VALORES DE VELOCIDAD
          var el=document.getElementById("formVelocidad").elements;
          var url = 'ResultadosV.html#'+ 1 + "#" + el["masa"].value+"#"+el["unidadesMasa"].value+"#"+el["altura"].value+"#"+
                    el["unidadesAltura"].value + "#" + el["distanciaFreno"].value + "#" + el["unidadesDistancia"].value + "#"+
@@ -28,11 +28,16 @@ function comprobarNum(valorIngreso) {
          window.location.replace(url);
 
      }
-    function valoresTiempo (){
-
+    function valoresTiempo (){ // CONCATENA LOS VALORES DE TIEMPO
+        var el2 = document.getElementById("formTiempo").elements;
+        var url2 = 'ResultadosT.html#'+ 2 + "#" + el2["masa"].value+"#"+el2["unidadesMasa"].value+"#"+el2["altura"].value+"#"+
+            el2["unidadesAltura"].value + "#" + el2["distanciaFreno"].value + "#" + el2["unidadesDistancia"].value + "#"+
+            el2["CoeficienteRozamiento"].value + "#" + el2["CoeficienteElasticidad"].value + "#" + el2["Radio"].value + "#"+
+            el2["unidadesRadio"].value;
+        window.location.replace(url2);
     }
  
-    function valoresEnergia (){
+    function valoresEnergia (){    //CONCATENA LOS VALORES DE ENERGIA
          var el=document.getElementById("formEnergia").elements;
          var url = 'ResultadosE.html#'+ 3 + "#" + el["masa"].value+"#"+el["unidadesMasa"].value+"#"+el["altura"].value+"#"+
              el["unidadesAltura"].value + "#" + el["distanciaFreno"].value + "#" + el["unidadesDistancia"].value + "#"+
@@ -41,7 +46,7 @@ function comprobarNum(valorIngreso) {
 
     }
 
-    function valoresResorte(){
+    function valoresResorte(){    //CONCATENA LOS VALORES DE RESORTE
         var el2 = document.getElementById("formResorte").elements;
         var url2 = 'ResultadosR.html#'+ 4 + "#" + el2["masa"].value+"#"+el2["unidadesMasa"].value+"#"+el2["altura"].value+"#"+
             el2["unidadesAltura"].value + "#" + el2["distanciaFreno"].value + "#" + el2["unidadesDistancia"].value + "#"+
@@ -50,7 +55,7 @@ function comprobarNum(valorIngreso) {
 
     }
  
-  function cargarResultado() {   // AGREGADO!!!!
+  function cargarResultado() {   // CALCULA TODOS LOS RESULTADOS Y GENERA EL ARRAY DE VALORES
 
         var g= 9.8;
       
@@ -63,7 +68,8 @@ function comprobarNum(valorIngreso) {
         var uDistancia = (myArr[7]); // Unidades distancia
         var coefRozamiento = (myArr[8]); // Coeficiente de rozamiento
         var coefElasticidad = (myArr[9]); // Coeficiente de elasticidad
-
+        var radio = (myArr[10]); //  Radio de la Rampa
+        var uRadio = (myArr[11]); // Unidades de Radio
 
 
        if (uMasa != "gramo"){
@@ -74,31 +80,38 @@ function comprobarNum(valorIngreso) {
       }
       if(uDistancia != "metro"){
           distanciaFreno = convertirUnidades(distanciaFreno, uDistancia);
+      }
 
+      if (uRadio != "metro"){
+          radio = convertirUnidades(radio, uRadio);
       }
 
 
-        var vB = Math.sqrt((2*g*altura));
-        var vC = Math.sqrt((vB*vB)-(2*coefRozamiento*distanciaFreno*g));
-        var x = Math.sqrt((2*masa*g*(altura-(coefRozamiento*distanciaFreno)))/coefElasticidad);
-        var energiaAB= masa*g*altura;
-        var energiaCR = energiaAB - (coefRozamiento*masa*g*distanciaFreno);
+        var vB = Math.sqrt((2*g*altura));  //VELOCIDAD EN B
+        var vC = Math.sqrt((vB*vB)-(2*coefRozamiento*distanciaFreno*g)); //VELOCIDAD EN C
+        var x = Math.sqrt((2*masa*g*(altura-(coefRozamiento*distanciaFreno)))/coefElasticidad); //DISTANCIA ACORTADA DEL RESORTE
+        var energiaAB= masa*g*altura;  //ENERGIA EN A Y EN B
+        var energiaCR = energiaAB - (coefRozamiento*masa*g*distanciaFreno); //ENERGIA EN C Y EN EL RESORTE
+        var tiempoB = 5;
+        var tiempoC = 8;
+        var tiempoR = 7;
+
 
 
        
       if(myArr[1] == 1) {
           document.getElementById("VelocidadB").value = vB;
           document.getElementById("VelocidadC").value = vC;
-      }else if(myArr[1] ==2){
-            // llamar al Tiempo!
+      }else if(myArr[1] == 2){
+          document.getElementById("TiempoB").value = tiempoB;
+          document.getElementById("TiempoC").value = tiempoC;
+          document.getElementById("TiempoR").value = tiempoR;
       }else if(myArr[1] == 3){
           document.getElementById("EnergiaAB").value = energiaAB;
           document.getElementById("EnergiaCR").value = energiaCR;
       }else if(myArr[1] == 4){
           document.getElementById("xResorte").value = x;
       }
-
-
 
   }
 
